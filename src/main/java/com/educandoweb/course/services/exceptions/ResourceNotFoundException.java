a05f54ba1a50c05995e19d9ddd758b7f650d339e
Leo Abrantes <leoabrantesmelo@gmail.com>
2022-02-22 19:16:40 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/educandoweb/course/services/exceptions/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package services.exceptions;
+package com.educandoweb.course.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
